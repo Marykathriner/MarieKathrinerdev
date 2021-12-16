9fe13d345f8e19ec0205971159cf14d7f4e738b1
Marykathriner <91670061+Marykathriner@users.noreply.github.com>
2021-12-16 12:23:06 +0100

    Edited conetent added hover

---
+++ script.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Hi, Marie here.';
+var speed = 130;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("introType").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeWriter);
